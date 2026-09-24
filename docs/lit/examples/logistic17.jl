476f45b771ec48c1e310563466a7c63d168881f5
Jeff Fessler <JeffFessler@users.noreply.github.com>
2026-09-23 22:37:41 -0400

    Add logistic regression for digits (#7)
    * show test error
    
    * rm ADTypes
    
    * add ForwardDiff
    
    * Add logistic regression for digits
    
    * show p surface

--- docs/lit/examples/logistic17.jl
+++ docs/lit/examples/logistic17.jl
@@ -1,8 +1,8 @@
 #=
-# [QDA demo](@id qda17)
+# [Logistic regression demo](@id logistic17)
 
 Illustrate
-[Quadratic discriminant analysis (QDA)](https://en.wikipedia.org/wiki/Quadratic_classifier#Quadratic_discriminant_analysis)
+[logistic regression](https://en.wikipedia.org/wiki/Logistic_regression)
 with MNIST hand-written digit images
 in Julia.
 =#
@@ -21,11 +21,14 @@ if you are using any of the following packages for the first time.
 if false
     import Pkg
     Pkg.add([
+        "ADTypes"
+        "ForwardDiff"
         "InteractiveUtils"
         "LaTeXStrings"
         "LinearAlgebra"
         "MIRTjim"
         "MLDatasets"
+        "Optim"
         "Plots"
         "Statistics"
     ])
@@ -35,11 +38,14 @@ end
 # Tell Julia to use the following packages.
 # Run `Pkg.add()` in the preceding code block first, if needed.
 
+using ADTypes: AutoForwardDiff
+import ForwardDiff
 using InteractiveUtils: versioninfo
 using LaTeXStrings: @L_str, latexstring
 using LinearAlgebra: svd, det
 using MIRTjim: jim, prompt
 using MLDatasets: MNIST
+using Optim: optimize, LBFGS, minimizer
 using Plots: default, gui, savefig, plot, plot!, scatter!, RGB, cgrad
 using Random: randperm, seed!
 using Statistics: mean
@@ -58,7 +64,7 @@ Read the MNIST data for some handwritten digits.
 This code will automatically download the data from web if needed
 and put it in a folder like: `~/.julia/datadeps/MNIST/`.
 =#
-if !@isdefined(data) || true
+if !@isdefined(data) # || true
     digitn = [1,7]
     isinteractive() || (ENV["DATADEPS_ALWAYS_ACCEPT"] = true) # avoid prompt
     dataset = MNIST(Float32, :train)
@@ -94,11 +100,9 @@ dtrain = data[:,:,tmp[itrain],:]
 dvalid = data[:,:,tmp[ivalid],:]
 dtest1 = data[:,:,tmp[itest1],:];
 
-# Sample mean training image:
+# Sample means of training images:
 dmean = sum(dtrain, dims = 3:4) / ntrain / ndigit
 pm = jim(dmean; title="Mean image")
-
-## savefig(pm, "lda$digit_str-mean.pdf")
 
 
 #=
@@ -112,8 +116,6 @@ U = svd(X).U[:,1:K];
 # Show basis vectors
 tmp = reshape(U, nx, ny, K)
 pu = jim(tmp; title="Basis functions, K=$K", color=:cividis, size=(700,400))
-
-## savefig(pu, "lda$digit_str-u.pdf")
 
 
 #=
@@ -135,8 +137,8 @@ args = (;
  size = (550, 500),
 )
 
-petr = plot(; title="Train data", args...)
-pete = plot(; title="Test data", args...)
+petr = plot(; title = "Train data", args...)
+pete = plot(; title = "Test data", args...)
 
 colors = (:blue, :red)
 for id in 1:ndigit
@@ -147,111 +149,103 @@ for id in 1:ndigit
 end
 pp = plot(petr, pete; size = (950, 500))
 
-## savefig(pp, "lda$digit_str-data.pdf")
-
 #
 prompt()
 
 #=
-## Illustrate maximum-likelihood estimates
+## Logistic classifier design
+First set up the ERM cost function
+(regularized negative log-likelihood)
+using the {0,1} label formulation.
 =#
 
-# Means
-means = [vec(mean(X; dims=2)) for X in eachslice(Xtrain, dims=3)]
-function plot_means!(p; dolabel::Bool = true)
-    for id in 1:ndigit
-        scatter!(p, [means[id][1]], [means[id][2]]; color = colors[id],
-            markerstrokecolor = :black, marker = :square, markersize = 7,
-            label = dolabel ? latexstring("\\mathbf{μ}_$(digitn[id])") : "",
-        )
-    end
-    return p
-end
-plot_means!(petr)
-plot!(petr, title = "Train data with sample means")
-petr
-
-#
-prompt()
-
-## savefig(petr, "lda$digit_str-means.pdf")
-
-
-function add_ellipse!(p, mean, Σ; kwargs...)
-    z = hcat([collect(sincos(t)) for t in range(0, 2π, 101)]...)
-    xc = sqrt(2Σ) * z
-    return plot!(p, mean[1] .+ xc[1,:], mean[2] .+ xc[2,:]; color=:black, kwargs...)
+# data matrices should be d × nₖ
+function model_setup(data0::AbstractMatrix, data1::AbstractMatrix, reg::Real)
+    n0, n1 = size(data0,2), size(data1,2)
+    n = n0 + n1 # total number of training samples
+    data0_bar = [ones(1, n0); data0]
+    data1_bar = [ones(1, n1); data1]
+    nl_sigma1(x) = log(1 + exp(-x)) # negative of log of σ
+    nl_sigma0(x) = log(1 + exp(+x)) # negative of log of 1 - σ
+    cost(θ) = (1/n) * (
+        sum(nl_sigma0, θ' * data0_bar) +
+        sum(nl_sigma1, θ' * data1_bar)) + reg/2 * sum(abs2, θ)
+    return cost
 end;
 
+# No regularization for now because low dimensional space:
+erm_cost = model_setup(Xtrain[:,:,1], Xtrain[:,:,2], 0);
 
-# Plot de-meaned data:
-Xdemean = cat([Xtrain[:,:,id] .- means[id] for id in 1:ndigit]..., dims=3)
 
-pdm = Vector{Any}(undef, ndigit)
-Σ = Vector{Matrix{<:Number}}(undef, ndigit)
-for id in 1:ndigit
-    p = plot(; title="Train data de-meaned, k=$id", args...)
-    scatter!(p, Xdemean[1,:,id], Xdemean[2,:,id],
-        color = colors[id], label = "$(digitn[id])",
-    )
-
-    local tmp = Xdemean[:,:,id]
-    Σ[id] = tmp * tmp' / size(tmp,2) # sample covariance
-
-    add_ellipse!(p, zeros(2), Σ[id])
-    pdm[id] = p
-end
-pc = plot(pdm..., size = (950, 500))
+#=
+## Explore cost function
+Make 2D plot for ``θ = [0, w_1, w_2]``
+=#
+ws = range(-0, 3, 31)
+cost2 = [erm_cost([0, w1, w2]) for w1 in ws, w2 in ws]
+pc = jim(ws, ws, cost2; title = "J(θ)",
+ xlabel = L"w_1", ylabel = L"w_2", color = :viridis, yflip=:false)
+tmp = argmin(cost2)
+scatter!(pc, [ws[tmp[1]]], [ws[tmp[2]]], color = :white) # show minimizer
 
 #
 prompt()
 
-## savefig(pc, "qda$digit_str-cov.pdf")
+
+#=
+## Minimize via LBFGS quasi-Newton (QN) method
+The "L" is for limited memory
+which is unimportant for this d=2 setting,
+but is useful when applying QN
+to the original data.
+=#
+θ0 = zeros(3)
+opt = optimize(erm_cost, θ0, LBFGS(); autodiff = AutoForwardDiff())
+θhat = minimizer(opt)
+
+
+# Logistic regression discriminant function
+function lr_discriminant(x::AbstractVector; θ::Vector = θhat)
+    return θhat' * [1; x]
+end;
+
+# Logistic regression classifier that returns digit labels (not 0,1)
+function lr_classify1(x::AbstractVector; θ::Vector = θhat)
+    return lr_discriminant(x; θ) ≥ 0 ? digitn[2] : digitn[1] # labels!
+end;
+
+#src lr_classify1([0,0]) # test
 
 
 #=
-## QDA classifier
-brute force with argmax
+## Plot decision boundary
 =#
-
-sqrtΣinv = @. inv(sqrt(Σ))
-function qda_classify1(
-    x::AbstractVector;
-    means::Vector{<:Vector} = means,
-    sqrtΣinv::Vector{<:Matrix} = sqrtΣinv,
-    prob::AbstractVector = fill(0.5, length(means)),
-)
-    score = copy(prob) # πₖ
-    for id in 1:length(means)
-        r = sqrtΣinv[id] * (x - means[id]) # whitened residual
-        score[id] *= det(sqrtΣinv[id]) * exp.(-(1/2) * sum(abs2, r))
-    end
-    return digitn[argmax(score)]
-end
-#src qda_classify1([0,0]) # test
-
-
 α = 0.2
 color = cgrad([RGB(1-α, 1-α, 1), :black, RGB(1, 1-α, 1-α)])
 x1_range = range(-6, 6, 221)
 x2_range = range(-6, 6, 223)
-function qda_plot(train_error::Real = NaN, test_error::Real = NaN;
-    classifier::Function = qda_classify1,
+function lr_plot(train_error::Real = NaN, test_error::Real = NaN;
+    classifier::Function = lr_classify1,
     title::AbstractString =
-        "QDA train error=$train_error %, test error = $test_error %",
-    Σs::Vector{<:Any} = Σ,
+        "L.R. train error=$train_error %, test error = $test_error %",
+    θ::Vector = θhat,
 )
-    tmp = [classifier([x1; x2]) for x1 in x1_range, x2 in x2_range]
+#src tmp = [classifier([x1; x2]) for x1 in x1_range, x2 in x2_range]
+    tmp = [lr_discriminant([x1; x2]) for x1 in x1_range, x2 in x2_range]
+    sigma(x) = 1 / (1 + exp(-x))
+    tmp = sigma.(tmp)
+
     p = jim(x1_range, x2_range, tmp; color, title, prompt = false,
-        colorbar_ticks = digitn, args...)
+        clim = (0,1), colorbar_ticks = 0:0.5:1, # digitn,
+        args...)
     for id in 1:ndigit
         scatter!(p, Xtrain[1,:,id], Xtrain[2,:,id],
             color = colors[id],
             label = "$(digitn[id])",
         )
-        add_ellipse!(p, means[id], Σs[id])
     end
-    plot_means!(p, dolabel = false)
+    boundary = (-θ[1] .- θ[2] * x1_range) / θ[3]
+    plot!(p, x1_range, boundary, color = :magenta) # decision boundary
     return p
 end;
 
@@ -260,7 +254,7 @@ end;
 ## Classification errors
 for train / validate / test
 =#
-function errors(data, label; classifier::Function = qda_classify1)
+function errors(data, label; classifier::Function = lr_classify1)
     data = reshape(data, K, :) # (d, n)
     err = count(classifier.(eachcol(data)) .!= label) / size(data, 2)
     return round(100 * err; sigdigits = 3)
@@ -268,15 +262,16 @@ end
 train_error = errors(Xtrain, ytrain)
 valid_error = errors(Xvalid, yvalid)
 test1_error = errors(Xtest1, ytest1)
-err1 = [ train_error valid_error test1_error ]
+err1 = [train_error valid_error test1_error]
 
 
 #=
 ## Plot data and decision regions:
 =#
-p0 = qda_plot(train_error, test1_error)
+p0 = lr_plot(train_error, test1_error)
+
 
 #
 prompt()
 
-## savefig(p0, "qda$digit_str-v1.pdf")
+## savefig(p0, "lr$digit_str-v1.pdf")
